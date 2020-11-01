f65cc85a55201ffc06d494a579ea829238be4b85
Vikram S <vikram.senthil@outlook.com>
2020-11-02 00:10:40 +0530

    Inital Commit

---
+++ src/main/java/net/vikram/springboot/curdrestfulwebservices/SpringbootCurdRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package net.vikram.springboot.curdrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCurdRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCurdRestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/vikram/springboot/curdrestfulwebservices/SpringbootCurdRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vikram.springboot.curdrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCurdRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
